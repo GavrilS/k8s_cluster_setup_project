1d0371e6aacc9722fe591a22300c16f83a8ddf93
GavrilS <gavr1lll@abv.bg>
2026-08-23 13:46:15 +0300

    Added verification commands to confirm the setup is properly completed

--- tools/setup_workstation.sh
+++ tools/setup_workstation.sh
@@ -12,3 +12,7 @@ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stabl
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 rm kubectl
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+
+echo "Test everything is set up properly"
+kubectl get nodes
+helm list -A
