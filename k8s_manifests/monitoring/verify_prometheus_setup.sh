f9a7ca26090974e102d078e3eddeb35a894df421
GavrilS <gavr1lll@abv.bg>
2026-08-27 22:06:30 +0300

    Fixed a typo

--- k8s_manifests/monitoring/verify_prometheus_setup.sh
+++ k8s_manifests/monitoring/verify_prometheus_setup.sh
@@ -3,4 +3,4 @@
 kubectl get pods -n monitoring
 kubectl get pvc -n monitoring
 kubectl get secret grafana-admin-secret -n monitoring
-kubectl get pods -n monitoring -1 app.kubernetes.io/name=grafana
+kubectl get pods -n monitoring -l app.kubernetes.io/name=grafana
